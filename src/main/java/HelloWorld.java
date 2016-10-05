c56f675ad2f8a32ed99aeaa211e5b03d788ae9c7
Kenneth Lui <k.lui@ucl.ac.uk>
2016-10-05 16:48:43 +0100

    Parse without Facade and use TikaInputStream

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,16 +1,32 @@
-import java.io.InputStream;
-import org.apache.tika.Tika;
-
 import java.io.IOException;
+import java.io.InputStream;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
 public class HelloWorld {
 
     public static void main(String[] args) throws IOException, SAXException, TikaException {
-        Tika tika = new Tika();
-        try (InputStream stream = HelloWorld.class.getResourceAsStream(args[0])) {
-            System.out.println(tika.parseToString(stream));
+        for (String s: args) {
+            try (InputStream stream = HelloWorld.class.getResourceAsStream(s);
+                 TikaInputStream tikaStream = TikaInputStream.get(stream)) {
+                Parser parser = new AutoDetectParser();
+                BodyContentHandler handler = new BodyContentHandler();
+                Metadata metadata = new Metadata();
+                ParseContext context = new ParseContext();
+
+                //parsing the file
+                parser.parse(tikaStream, handler, metadata, context);
+                System.out.println("File content : " + handler.toString());
+
+
+                System.out.println("============================");
+            }
         }
     }
 
