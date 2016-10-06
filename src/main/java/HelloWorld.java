7bcba224eb086ab705606a262dd590ad1172233c
Kenneth Lui <k.lui@ucl.ac.uk>
2016-10-06 13:27:12 +0100

    It does seem that OCR can be performed by AutoDetectParser whenever TIFF is in the right format. But we cannot check which parser is really used behind the scene..

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,51 +4,38 @@ import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
-import org.apache.tika.mime.MediaTypeRegistry;
 import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.DefaultParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
-import org.apache.tika.config.TikaConfig;
-import org.apache.tika.parser.ocr.TesseractOCRConfig;
-import org.apache.tika.parser.ocr.TesseractOCRParser;
 
 
 public class HelloWorld {
 
     public static void main(String[] args) throws IOException, SAXException, TikaException {
         for (String s: args) {
+            System.out.println("Parsing " + s);
             try (InputStream stream = HelloWorld.class.getResourceAsStream(s);
                  TikaInputStream tikaStream = TikaInputStream.get(stream)) {
 
-                // Try to run this for testTiff.tiff
-                Parser parser = new TesseractOCRParser();
+                // Try to run this for testTiff_noAlpha.tiff
+                Parser autoParser = new AutoDetectParser();
                 BodyContentHandler handler = new BodyContentHandler();
                 Metadata metadata = new Metadata();
                 ParseContext context = new ParseContext();
 
-                // Enable Tesseract
-                TesseractOCRConfig ocrconfig = new TesseractOCRConfig(
-                    HelloWorld.class.getResourceAsStream(
-                        "TesseractOCRConfig.properties"));
-                System.out.printf("TesseractOCRConfig:\n%s\n%d %d\n%s\n%s\n%s\n%d\n",
-                                  ocrconfig.getLanguage(),
-                                  ocrconfig.getMaxFileSizeToOcr(),
-                                  ocrconfig.getMinFileSizeToOcr(),
-                                  ocrconfig.getPageSegMode(),
-                                  ocrconfig.getTessdataPath(),
-                                  ocrconfig.getTesseractPath(),
-                                  ocrconfig.getTimeout());
-
-                context.set(TesseractOCRConfig.class, ocrconfig);
-                // Meta name: [X-Parsed-By], Meta value: [org.apache.tika.parser.EmptyParser]
-
                 //parsing the file
-                parser.parse(tikaStream, handler, metadata, context);
+                autoParser.parse(tikaStream, handler, metadata, context);
                 System.out.println("File content : " + handler.toString());
                 printMeta(metadata);
+
+                MediaType mt = MediaType.image("tiff");
+                Parser tiffParser = ((AutoDetectParser) autoParser)
+                                    .getParsers(context).get(mt);
+                System.out.println("Parser for tiff: " +
+                                   tiffParser.getClass().getName());
 
                 System.out.println("============================");
             }
