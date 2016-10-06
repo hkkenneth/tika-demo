c48b5b05751a370d8ea2dbe29859949b2fdc9f8b
Kenneth Lui <k.lui@ucl.ac.uk>
2016-10-06 13:33:15 +0100

    Use tika config to force DefaultParser not to handle TIFF

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.InputStream;
+import org.apache.tika.config.TikaConfig;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
@@ -20,11 +21,12 @@ public class HelloWorld {
             try (InputStream stream = HelloWorld.class.getResourceAsStream(s);
                  TikaInputStream tikaStream = TikaInputStream.get(stream)) {
 
-                // Try to run this for testTiff_noAlpha.tiff
-                Parser autoParser = new AutoDetectParser();
+                TikaConfig config = new TikaConfig(HelloWorld.class.getResource("tika-config.xml"));
+                Parser autoParser = new AutoDetectParser(config);
                 BodyContentHandler handler = new BodyContentHandler();
                 Metadata metadata = new Metadata();
                 ParseContext context = new ParseContext();
+                context.set(TikaConfig.class, config);
 
                 //parsing the file
                 autoParser.parse(tikaStream, handler, metadata, context);
@@ -34,8 +36,13 @@ public class HelloWorld {
                 MediaType mt = MediaType.image("tiff");
                 Parser tiffParser = ((AutoDetectParser) autoParser)
                                     .getParsers(context).get(mt);
-                System.out.println("Parser for tiff: " +
-                                   tiffParser.getClass().getName());
+                if (tiffParser == null) {
+                    System.out.println("Parser for tiff is NULL!");
+                } else {
+                    System.out.println("Parser for tiff: " +
+                                       tiffParser.getClass().getName());
+                }
+
 
                 System.out.println("============================");
             }
