a1a40b4f81f517450d67b400bd6693f4a7960bf9
Kenneth Lui <k.lui@ucl.ac.uk>
2017-05-10 01:42:40 +0100

    Final

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,49 +9,52 @@ import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.DefaultParser;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.Parser;
+import org.apache.tika.parser.pdf.PDFParser;
+import org.apache.tika.parser.pdf.PDFParserConfig;
 import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.ToXMLContentHandler;
 import org.xml.sax.SAXException;
 
+import java.io.File;
+import java.io.FileInputStream;
 
 public class HelloWorld {
 
     public static void main(String[] args) throws IOException, SAXException, TikaException {
         for (String s: args) {
             System.out.println("Parsing " + s);
-            try (InputStream stream = HelloWorld.class.getResourceAsStream(s);
+            //InputStream stream = HelloWorld.class.getResourceAsStream(s);
+            try (InputStream stream = new FileInputStream(new File(s));
                  TikaInputStream tikaStream = TikaInputStream.get(stream)) {
 
                 TikaConfig config = new TikaConfig(HelloWorld.class.getResource("tika-config.xml"));
-                Parser autoParser = new AutoDetectParser(config);
+                PDFParser pdfParser = new PDFParser();
+                PDFParserConfig parserConfig = new PDFParserConfig();
+                parserConfig.setExtractAcroFormContent(true);
+                parserConfig.setIfXFAExtractOnlyXFA(false);
+                //parserConfig.setOcrStrategy(PDFParserConfig.OCR_STRATEGY.OCR_AND_TEXT_EXTRACTION);
+                parserConfig.setOcrStrategy(PDFParserConfig.OCR_STRATEGY.NO_OCR);
+                //parserConfig.setOcrStrategy(PDFParserConfig.OCR_STRATEGY.OCR_ONLY);
+                pdfParser.setPDFParserConfig(parserConfig);
                 BodyContentHandler handler = new BodyContentHandler();
                 Metadata metadata = new Metadata();
                 ParseContext context = new ParseContext();
-                context.set(TikaConfig.class, config);
+                //context.set(TikaConfig.class, config);
+                context.set(PDFParserConfig.class, parserConfig);
 
+                ToXMLContentHandler xmlHandler = new ToXMLContentHandler();
                 //parsing the file
-                autoParser.parse(tikaStream, handler, metadata, context);
-                System.out.println("File content : " + handler.toString());
+                pdfParser.parse(tikaStream, xmlHandler, metadata, context);
+                System.out.println("File content : " + xmlHandler.toString());
+
                 printMeta(metadata);
-
-                MediaType mt = MediaType.image("tiff");
-                Parser tiffParser = ((AutoDetectParser) autoParser)
-                                    .getParsers(context).get(mt);
-                if (tiffParser == null) {
-                    System.out.println("Parser for tiff is NULL!");
-                } else {
-                    System.out.println("Parser for tiff: " +
-                                       tiffParser.getClass().getName());
-                }
-
-
-                System.out.println("============================");
             }
         }
     }
 
     private static void printMeta(Metadata metadata) {
         for (String name: metadata.names()) {
-            System.out.printf("Meta name: [%s], Meta value: [%s]\n", name,
+            System.err.printf("Meta name: [%s], Meta value: [%s]\n", name,
                               metadata.get(name));
         }
     }
